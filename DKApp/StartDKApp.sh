1854bcc6c38aad05fc31b64d76818c74728de8f9
Helmut Theis <helmut.theis@hypermediasystems.de>
2018-12-27 17:46:37 +0100

    Add comments to start script

--- DKApp/StartDKApp.sh
+++ DKApp/StartDKApp.sh
@@ -1,3 +1,5 @@
 #
-mono /volume1/util/dkapp/DKApp.exe -path /volume1/dkapp
-# sudo mono-service -l:/tmp/DKApp.lock ./DKApp.exe
+mono /volume1/util/dkapp/DKApp.exe -path /volume1/dkapp -noservice
+# sudo /var/packages/EmbyServer/target/mono/mono-service2 -l:/tmp/DKApp.lock ./DKApp.exe
+# cat /tmp/DKApp.lock
+# kill the process id from /tmp/DKApp.lock
